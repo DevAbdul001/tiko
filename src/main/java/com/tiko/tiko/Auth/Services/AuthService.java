cb069e4450052064d0d293070983472856667ac3
DevAbdul001 <brianpits9@gmail.com>
2026-06-27 17:33:17 +0300

    chore : add login service method

--- src/main/java/com/tiko/tiko/Auth/Services/AuthService.java
+++ src/main/java/com/tiko/tiko/Auth/Services/AuthService.java
@@ -1,11 +1,14 @@
 package com.tiko.tiko.Auth.Services;
 
+import com.tiko.tiko.Auth.DTO.AuthResponseDTO;
+import com.tiko.tiko.Auth.DTO.LoginDTO;
 import com.tiko.tiko.Auth.DTO.RegisterDTO;
 import com.tiko.tiko.Users.Entity.User;
 import com.tiko.tiko.Users.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class AuthService {
@@ -31,6 +34,23 @@ public class AuthService {
         userRepo.save(user);
 
         return user.getId();
+    }
+
+    public AuthResponseDTO login(LoginDTO dto){
+        User user = userRepo.findByEmail(dto.email())
+                .orElseThrow(()-> new RuntimeException("Email doesnt exist"));
+
+        boolean matches = passwordEncoder.matches(dto.password(), user.getPasswordHash());
+
+        if(!matches){
+            throw  new RuntimeException("Invalid credentials");
+        }
+
+        return new AuthResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail()
+        );
     }
 
 }
