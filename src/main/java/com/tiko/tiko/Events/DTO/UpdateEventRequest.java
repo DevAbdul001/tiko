7dd2d252186a994e29f8e8153cc5eb5dcc39151b
DevAbdul001 <abdurahmanali363@gmail.com>
2026-07-04 22:49:11 +0300

    chore : add eventUpdateResquest dto chore : add update event method

---
+++ src/main/java/com/tiko/tiko/Events/DTO/UpdateEventRequest.java
@@ -0,0 +1,13 @@
+package com.tiko.tiko.Events.DTO;
+
+import com.tiko.tiko.Events.Utils.EventStatus;
+
+import java.time.LocalDateTime;
+
+public record UpdateEventRequest(
+        EventStatus status,
+        String location,
+        LocalDateTime date,
+        Long capacity,
+        String imageUrl
+) {}
