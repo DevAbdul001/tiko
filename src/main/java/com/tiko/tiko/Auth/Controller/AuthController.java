d2e3f1b0e9117d9c4f1910d334ca0f61dba2e6bb
DevAbdul001 <abdurahmanali363@gmail.com>
2026-07-08 22:11:26 +0300

    chore : add a utils service for creating cookies refactor : refactor refresh endpoint

--- src/main/java/com/tiko/tiko/Auth/Controller/AuthController.java
+++ src/main/java/com/tiko/tiko/Auth/Controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.tiko.tiko.Auth.Controller;
 
+import com.tiko.tiko.Auth.DTO.AuthResponseDTO;
+import com.tiko.tiko.Auth.DTO.LoginDTO;
 import com.tiko.tiko.Auth.DTO.RegisterDTO;
 import com.tiko.tiko.Auth.Services.AuthService;
+import com.tiko.tiko.Auth.Services.CookieService;
 import com.tiko.tiko.Auth.Services.JWTService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,6 +22,7 @@ public class AuthController {
 
     private final AuthService authService;
     private final JWTService jwtService;
+    private final CookieService cookieService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
@@ -23,6 +30,41 @@ public class AuthController {
             @RequestBody RegisterDTO dto
             ){
         authService.registerUser(dto);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthResponseDTO login(
+            @RequestBody LoginDTO dto ,
+            HttpServletResponse response
+            ) {
+        AuthResponseDTO result = authService.login(dto);
+        String accessToken = jwtService.generateAccessToken(result);
+        String refreshToken = jwtService.generateRefreshToken(result);
+
+        ResponseCookie accessCookie = cookieService.generateAccessCookie(accessToken);
+        ResponseCookie refreshCookie = cookieService.generateRefreshToken(refreshToken);
+
+        response.addHeader("Set-cookie", accessCookie.toString());
+        response.addHeader("Set-Cookie", refreshCookie.toString());
+
+        return result;
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<Void> refresh(
+            @CookieValue("refreshToken") String refreshToken,
+            HttpServletResponse response
+    ) {
+        Long userId = jwtService.extractUserId(refreshToken);
+        AuthResponseDTO dto = authService.fetchById(userId);
+
+        String accessToken = jwtService.generateAccessToken(dto);
+        ResponseCookie accessCookie = cookieService.generateAccessCookie(accessToken);
+
+        response.addHeader("Set-Cookie", accessCookie.toString());
+
+        return  ResponseEntity.ok().build();
     }
 
 
---
+++ src/main/java/com/tiko/tiko/Auth/Services/CookieService.java
@@ -0,0 +1,33 @@
+package com.tiko.tiko.Auth.Services;
+
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class CookieService {
+
+    private JWTService jwtService;
+
+    public ResponseCookie generateAccessCookie(String token){
+        return ResponseCookie.from("accessToken",  token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(15 * 60)
+                .sameSite("true")
+                .build();
+    }
+
+    public ResponseCookie generateRefreshToken(String token){
+        return ResponseCookie.from("refreshToken", token)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60 * 1000)
+                .sameSite("true")
+                .build();
+    }
+}
